d612e9d6fbaad8e91e78660add728e2900c8c7bf
xinyu-hou <61299181+xinyu-hou@users.noreply.github.com>
2023-03-31 13:32:35 -0400

    a9 MongoDB

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import { config } from 'dotenv';
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
+
+config();
+const localConnectionString = 'mongodb://127.0.0.1:27017/tuiter';
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || localConnectionString;
+console.log(CONNECTION_STRING);
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors());
 app.use(express.json());
